82e52c3dc12d7b1a416548a00350b59eb1efd083
Kaustubh Dhasmana <kdhasmana@EX2INDIA.LOCAL>
2020-02-10 10:32:30 +0530

    Cleans up the logic

--- game.ts
+++ game.ts
@@ -1,114 +1,69 @@
-type RPS = "rock" | "paper" | "scissors" | null;
-
-const icons = {
-   "rock": "👊",
-   "paper": "🖐",
-   "scissors": "✌"
+type RPS = 0 | 1 | 2 | null; // Rock, Paper, Scissor
+type res = 0 | 1 | 2; // Win, lose, draw
+enum symbols  {
+   "rock",
+   "paper",
+   "scissors",
 };
+
 
 class Game{
    p1Symbol: RPS;
    p2Symbol: RPS;
-   scores: number[] = [0,0,0];
-   // computerChoiceLabel: HTMLSpanElement;
-   // resultLabel: HTMLSpanElement;
+   scores: number[] = [0, 0, 0];
+   static gameMatrix: RPS[][] = [[2, 1, 0], [0, 2, 1], [1, 0, 2]];
+   static plays = [
+      ["You won", "win"],
+      ["You lost", "lose"],
+      ["Draw", "draw"]
+   ];
    choiceButtons: NodeListOf<HTMLButtonElement>;
    scoreLabels: NodeListOf<HTMLSpanElement>;
    
    constructor(){
-   //   this.p1Container = p1Container;
-   //   this.p2Container = p2Container; 
-      // this.computerChoiceLabel = document.getElementById('computer-choice');
-      // this.resultLabel = document.getElementById('result');
       this.choiceButtons = document.querySelectorAll('button[id^=p1-]');
       this.scoreLabels = document.querySelectorAll('span[id$=-score]');
       
-      // Set up listeners for buttons
+      // Set up click listeners for buttons
       this.choiceButtons.forEach((button: HTMLButtonElement) => {
-         button.addEventListener('click', this.eventHandler.bind(this));
+         button.addEventListener('click', this.clickEventHandler.bind(this));
       });
+      // Set up key events
+      document.addEventListener('keydown', this.keyEventHandler);
    }
-   eventHandler(e: Event){
-      // console.log(this);
-      this.userTurn((e.target as HTMLButtonElement).dataset['symbol'] as RPS);
-      // console.log(e);
+   clickEventHandler(e: Event){
+      this.userTurn(parseInt((e.target as HTMLButtonElement).dataset['symbol']) as RPS);
+   }
+   keyEventHandler(e: KeyboardEvent){
+      let but: HTMLButtonElement = document.querySelector('button[data-key="' + e.key + '"]');
+      console.log(but);
+      if(but)
+         but.click();
    }
    reset(){
       this.p1Symbol = null;
       this.p2Symbol = null;
-
    }
    userTurn(choice: RPS){
-      console.log('User chose '+ choice);
       this.p1Symbol = choice;
       this.computerTurn();
    }
    computerTurn(){
-      const choice = Math.random() * 10
-      switch(true){
-         case choice <= 3.33:
-            this.p2Symbol = "rock";
-            break;
-         case choice > 3.33 && choice <= 6.66:
-            this.p2Symbol = "paper";
-            break;
-         case choice > 6.66:
-            this.p2Symbol = "scissors";
-            break;
-      }
-      console.log('Computer chose ' + this.p2Symbol);
+      const choice = Math.round(Math.random() * 2)
+      this.p2Symbol = choice as RPS;
       
       // Start the game
       this.evaluate();
    }
    evaluate(){
-      if(this.p1Symbol === this.p2Symbol)
-         this.displayResults(2);
-      else if(this.p1Symbol === 'rock'){
-         if(this.p2Symbol === 'paper')
-            this.displayResults(1);
-         else
-         this.displayResults(0);
-      }
-      else if(this.p1Symbol === 'paper'){
-         if(this.p2Symbol === 'scissors')
-            this.displayResults(1);
-         else
-         this.displayResults(0);
-      }
-      else if(this.p1Symbol === 'scissors'){
-         if(this.p2Symbol === 'rock')
-            this.displayResults(1);
-         else
-         this.displayResults(0);
-      }
+      const p1 = this.p1Symbol;
+      const p2 = this.p2Symbol;
+      this.displayResults(Game.gameMatrix[this.p1Symbol][this.p2Symbol]);
    }
-   displayResults(result: 0 | 1 | 2){
-      const strings = {
-         "res": '',
-         "comp": 'Computer chose ',
-         "class": ''
-      }
-      switch(result){
-         case 0:
-            strings.res = 'You won';
-            strings.comp += this.p2Symbol;
-            strings.class = 'win';
-            break;
-         case 1:
-            strings.res = 'You lost';
-            strings.comp += this.p2Symbol;
-            strings.class = 'lose';
-            break;
-         case 2:
-            strings.res = 'Draw';
-            strings.comp += this.p2Symbol + ' too!';
-            strings.class = 'draw';
-            break;
-      }
-      window["computer-choice"].textContent = strings.comp;
-      window["result"].className = strings.class;
-      window["result"].textContent = strings.res;
+   displayResults(result: res){
+      window["computer-choice"].textContent = 'Computer chose ' + symbols[this.p2Symbol];
+      window["result"].className = Game.plays[result][1];
+      window["result"].textContent = Game.plays[result][0];
       this.scoreLabels[result].textContent = String(++this.scores[result]);
    }
 }
